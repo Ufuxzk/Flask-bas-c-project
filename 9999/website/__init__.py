9f480b3fad21d63478af29fb82c09d4b807d547c
Ufuk Eren İnce <60223186+Ufuxzk@users.noreply.github.com>
2021-12-11 15:54:58 +0300

    Update __init__.py

--- 9999/website/__init__.py
+++ 9999/website/__init__.py
@@ -8,7 +8,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app  = Flask(__name__)
-    app.config['SECRET_KEY'] = "coüpqjwcpqwjcpqwjcopqwjcopqwj copqwjcopqjwopwqjco"
+    app.config['SECRET_KEY'] = "SECRET KEY"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
